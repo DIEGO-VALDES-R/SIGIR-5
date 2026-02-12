8e23a7d391003db0e599b8aabc9b1ecc70dcbd61
Diego Valdes <diegofernando@office365tl.onmicrosoft.com>
2026-02-12 16:09:10 -0500

    add usePersistFn hook

---
+++ client/src/hooks/usePersistFn.ts
@@ -0,0 +1,15 @@
+﻿import { useRef } from 'react';
+
+export function usePersistFn<T extends (...args: any[]) => any>(fn: T) {
+  const fnRef = useRef<T>(fn);
+  fnRef.current = fn;
+
+  const persistFn = useRef<T>();
+  if (!persistFn.current) {
+    persistFn.current = ((...args) => {
+      return fnRef.current(...args);
+    }) as T;
+  }
+
+  return persistFn.current;
+}
